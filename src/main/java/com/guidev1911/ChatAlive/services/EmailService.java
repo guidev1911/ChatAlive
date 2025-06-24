387f20785ca27459ea6b944bd41b4da7869c130f
guidev1911 <guilhermereal1911@gmail.com>
2025-06-24 00:37:13 -0300

    código de email em ciano

--- src/main/java/com/guidev1911/ChatAlive/services/EmailService.java
+++ src/main/java/com/guidev1911/ChatAlive/services/EmailService.java
@@ -20,7 +20,7 @@ public class EmailService {
             String htmlContent =
                     "<p>Olá,</p>" +
                             "<p>Você solicitou um código de confirmação para concluir seu cadastro no <strong>ChatAlive</strong>.</p>" +
-                            "<p><strong>Seu código de confirmação é:</strong> <span style='font-size: 16px; color: #28a745;'><strong>" + code + "</strong></span></p>" +
+                            "<p><strong>Seu código de confirmação é:</strong> <span style='font-size: 16px; color: #00FFFF;'><strong>" + code + "</strong></span></p>" +
                             "<p>⚠️ Este código é válido por apenas <strong>5 minutos</strong>.</p>" +
                             "<p style='color: #999;'>Caso você não tenha solicitado este código, por favor ignore esta mensagem.</p>" +
                             "<p>Atenciosamente,<br><em>Equipe ChatAlive</em></p>";
@@ -43,7 +43,7 @@ public class EmailService {
             String htmlContent =
                     "<p>Olá,</p>" +
                             "<p>Recebemos uma solicitação para redefinir a senha da sua conta no <strong>ChatAlive</strong>.</p>" +
-                            "<p><strong>Seu código de verificação é:</strong> <span style='font-size: 16px; color: #007bff;'><strong>" + code + "</strong></span></p>" +
+                            "<p><strong>Seu código de verificação é:</strong> <span style='font-size: 16px; color: #00FFFF;'><strong>" + code + "</strong></span></p>" +
                             "<p>⚠️ Este código é válido por apenas <strong>5 minutos</strong>.</p>" +
                             "<p style='color: #999;'>Caso você não tenha solicitado esta redefinição, por favor ignore esta mensagem.</p>" +
                             "<p>Atenciosamente,<br><em>Equipe ChatAlive</em></p>";
